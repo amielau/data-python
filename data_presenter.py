113182fd5f5770acfb3000f63dddb3663c41ee89
amielau <amielperkins@gmail.com>
2022-03-10 15:47:52 -0700

    everything completed

---
+++ data_presenter.py
@@ -0,0 +1,22 @@
+# open_file = open('CupcakeInvoices.csv')
+
+# for invoices in open_file:
+#     print (invoices)
+
+# for invoices in open_file:
+#     orders = invoices.split(',')
+#     print (orders[2])
+
+# for invoices in open_file:
+#     orders = invoices.split(',')
+#     total = int(orders[3]) * float(orders[4])
+#     print(total)
+
+# total = 0
+# for invoices in open_file:
+#     orders = invoices.split(',')
+#     total = total + (int(orders[3]) * float(orders[4]))
+#     print(total)
+
+# open_file(close)
+
